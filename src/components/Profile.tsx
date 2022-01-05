c9febddca35dd1b77e6fc0c0e2fa986c31557c39
JvPelai <jv.pelai@gmail.com>
2022-01-05 16:37:36 -0300

    initial config

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+
+import Image from "next/image"
+import profileImage from '../../public/assets/profile.jpg'
+import utilStyles from '../styles/utils.module.css';
+const Profile = () => {
+  return(
+   
+    <Image
+    priority
+    src={profileImage}
+    className={utilStyles.borderCircle}
+    alt='João Pelai'
+    width={264}
+    height={264}
+    />
+  
+  )
+}
+
+export default Profile
